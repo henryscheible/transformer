beb443aeab187f7c0c9051e3e82c7dcb056d63d7
Henry Scheible <henry.scheible@gmail.com>
2022-09-12 15:36:02 -0400

    refactor attention to subclass torch.nn.Module

--- attention.py
+++ attention.py
@@ -1,47 +1,55 @@
-from torch import Tensor
+from torch import Tensor, nn
 from torch.nn.functional import softmax
 import torch
 
 
-def scaled_dot_product_attention(q: Tensor, k: Tensor, v: Tensor) -> Tensor:
-    # q shape: batch_size, seq_length, d_k
-    # k shape: batch_size, seq_length, d_k
-    # v shape: batch_size, seq_length, d_v
-    d_k = q.size()[-1]
+class MultiHeadAttention(nn.Module):
+    def __init__(self, k, heads=8):
+        # k: dimension of queries, keys, values.
+        # k * heads: dimension of embeddings (to be inputted to the model)
+        super().__init__()
+        self.d_model, self.heads = k, heads
+        self.to_keys = nn.Linear(k, k * heads, bias=False)
+        self.to_queries = nn.Linear(k, k * heads, bias=False)
+        self.to_values = nn.Linear(k, k * heads, bias=False)
+        self.unify_heads = nn.Linear(k * heads, k, bias=False)
 
-    # Perform a batch matrix product on q and k.
-    # Dimension transform: [batch_size, seq_length, num_features] *
-    #                      [batch_size, num_features, seq_length] =
-    #                      [batch_size, seq_length, seq_length]
-    temp = q.bmm(q, k.transpose(1, 2))
+    def forward(self, q, k, v):
+        batch_size, seq_length, feature_dim = k.size()
+        h = self.heads
+        queries = self.to_queries(q).view(batch_size, seq_length, h, feature_dim)
+        keys = self.to_keys(k).view(batch_size, seq_length, h, feature_dim)
+        values = self.to_values(k).view(batch_size, seq_length, h, feature_dim)
 
-    # Calculate the scale and run the softmax operation
-    scale = d_k ** (1 / 2)
-    post_softmax = softmax(temp / scale, dim=-1)
-    return post_softmax.bmm(v)
+        # Combine head dimension with batch dimension so that dot products can be computed with bmm
+        queries = queries.transpose(1, 2).contiguous().view(batch_size * h, seq_length, feature_dim)
+        keys = keys.transpose(1, 2).contiguous().view(batch_size * h, seq_length, feature_dim)
+        values = values.transpose(1, 2).contiguous().view(batch_size * h, seq_length, feature_dim)
+
+        # matrix dimensions: seq_length, feature_dim * feature_dim, seq_length
+        # outputs: seq_length, seq_length
+        # batch size: batch_size * h
+        dot = torch.bmm(queries, keys.transpose(1, 2))
+
+        # Perform softmax on each row
+        dot = softmax(dot, dim=2)
+
+        # matrix dimensions: seq_length, seq_length * seq_length, feature_dim
+        # Then unpack batch_size * h dimension into 2 dimensions, producing original shape rank 4 tensor
+        out = torch.bmm(dot, values).view(batch_size, h, seq_length, feature_dim)
+
+        # Swap seq_length, h dimensions so that heads are together for each query
+        out = out.transpose(1, 2).contiguous().view(batch_size, seq_length, h, feature_dim)
+
+        # Unify heads
+        return self.unify_heads(out)
 
 
-def multi_head_attention(q: Tensor,
-                         k: Tensor,
-                         v: Tensor,
-                         weights_q: Tensor,
-                         weights_k: Tensor,
-                         weights_v: Tensor,
-                         weights_o: Tensor,
-                         h=8) -> Tensor:
-    # q shape: batch_size, seq_length, d_k
-    # k shape: batch_size, seq_length, d_k
-    # v shape: batch_size, seq_length, d_v
-    # weights_q shape: h, d_model, d_k
-    # weights_k shape: h, d_model, d_k
-    # weights_v shape: h, d_model, d_v
-    # weights_o shape: h * d_v, d_model
-    # d_k=d_v=d_model/h=64
-    head = list(h)
-    for i in range(h):
-        head[i] = scaled_dot_product_attention(
-            q.bmm(weights_q),
-            k.bmm(weights_k),
-            v.bmm(weights_v)
-        )
-    return torch.cat(head, dim=1).bmm(weights_o)
+class SelfAttention(nn.Module):
+
+    def __init__(self, k, heads=8):
+        super().__init__()
+        self.attention = MultiHeadAttention(k, heads=heads)
+
+    def forward(self, x):
+        return self.attention(x, x, x)
